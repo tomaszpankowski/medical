4c4069384e62f576c21cc972f95fdb9ea334dc91
tomaszpankowski <pankowski.email@gmail.com>
2022-01-21 12:44:12 +0100

    modified frontend

---
+++ src/views/news.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+import Container from "../../node_modules/react-bootstrap/Container";
+
+class News extends Component{
+    render(){
+        return(        
+            <Container fluid className="news-s1 d-flex align-items-center minh-100vh">
+                
+            </Container>    
+        );
+    }
+}
+
+export default News;
