beb5679935e502e90b6302f821c894a5fe2f10bc
tomaszpankowski <pankowski.email@gmail.com>
2022-03-28 09:51:02 +0200

    backend

--- src/components/message-error.js
+++ src/components/message-error.js
@@ -11,16 +11,16 @@ class MessageError extends Component {
     }
     render() {
         return (
-            <Container fluid className={"contact-s2 align-items-center p-0 py-5 d-flex minh-50vh " + this.props.classExt}>
+            <Container fluid className={"contact-s2 align-items-center p-0 py-5 border-top border-info d-flex minh-50vh " + this.props.classExt}>
                 <Row className="mx-auto text-center w-100">
                     <Col xs={10} md={6} className="mx-auto p-0 d-flex align-items-center">
-                        <Card className="mx-auto bg-light-gray opacity-8 border-dark-gray shadow">
-                            <Card.Header className="text-secondary text-start border-dark-gray">
+                        <Card className="mx-auto bg-dark opacity-9 border-light shadow">
+                            <Card.Header className="text-light text-start border-light">
                                 Message error!
                             </Card.Header>
-                            <Card.Body className="text-center text-secondary p-5">
+                            <Card.Body className="text-center text-light p-5">
                                 <p className="lead">Message was not sent!</p>
-                                <Button variant={"outline-secondary rounded-pill"} onClick={this.hndBack.bind(this)}>
+                                <Button variant={"outline-light rounded-pill"} onClick={this.hndBack.bind(this)}>
                                     back to Form
                                 </Button>
                             </Card.Body>
